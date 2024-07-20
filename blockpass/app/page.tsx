195a065d701112f1e7f0b34c0887c50146397ed5
Gursukh Sembi <gursukhsembi@gmail.com>
2024-07-20 20:44:38 +0100

    Im gonna kill myself

--- blockpass/app/page.tsx
+++ blockpass/app/page.tsx
@@ -5,22 +5,38 @@ import { Event } from "./types";
 
 const dummyEvent: Event = {
   name: "Vyoms Mom",
-  description: "In the garden of life, she stands so tall A guiding light, the warmest call. Her heart's a haven, pure and kind, Still, she's a fortress in her mind. She's a warrior, a champion, a queen, She a hoe.",
+  description:
+    "In the garden of life, she stands so tall A guiding light, the warmest call. Her heart's a haven, pure and kind, Still, she's a fortress in her mind. She's a warrior, a champion, a queen, She a hoe.",
   date: Date.now(),
   location: "Bedroom",
-  thumbnail: "https://images.unsplash.com/photo-1500382017468-9049fed747ef?fm=jpg&q=60&w=3000&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8NHx8ZmllbGR8ZW58MHx8MHx8fDA%3D"
-}
+  thumbnail:
+    "https://images.unsplash.com/photo-1500382017468-9049fed747ef?fm=jpg&q=60&w=3000&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8NHx8ZmllbGR8ZW58MHx8MHx8fDA%3D",
+};
 
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center pb-20">
-      <NavigationBar />
-      <h1 className=" cursor-default my-8 text-[32px]">Discover Events</h1>
-      <div className="grid grid-cols-[1fr_1fr_1fr] w-full max-w-[800px] gap-4">
-      <EventCard event={dummyEvent}  className="col-span-3" size="large"/>
-      <EventCard event={dummyEvent}  className="col-span-2"/>
-      <EventCard event={dummyEvent}  className=""/>
+    <main className="h-[100dvh]">
+      <div className="py-10">
+        {/* <h1 className="text-[50px] my-12 mx-auto text-center font-bold">BLOCKPASS</h1> */}
+        <div className="flex justify-between overflow-hidden mb-20 mx-auto">
+          <div className="w-[200px] h-[300px] bg-primary" />
+          <div className="w-[160px] h-[220px] bg-primary" />
+          <div className="w-[250px] h-[160px] bg-primary" />
+          <div className="w-[250px] h-[200px] bg-primary" />
+          <div className="w-[250px] h-[160px] bg-primary" />
+          <div className="w-[150px] h-[260px] bg-primary" />
+        </div>
+
+        <div className="grid grid-cols-[60%_40%] text-[48px] max-w-[1200px] mx-auto">
+          <div className="flex-col gap-2 flex">
+            <h1 className="font-light leading-[120%]">
+              Book tickets for your favourite artists with security
+            </h1>
+            <p className="font-extralight w-[80%] text-[16px]">Bla bla bla subtext. Bla bla bla subtext. Bla bla bla subtext. Bla bla bla subtext. Bla bla bla subtext. </p>
+          </div>
+        </div>
       </div>
+
     </main>
   );
 }
